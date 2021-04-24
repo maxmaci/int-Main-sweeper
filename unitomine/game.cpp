e8fae1368913a9f001d3ef0b9a4f55450fab026b
maxmaci <74024731+maxmaci@users.noreply.github.com>
2021-04-24 18:13:29 +0200

    Update 24/04

--- unitomine/game.cpp
+++ unitomine/game.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>		// per input/output
 #include <vector>		// classe vector
 #include <string>
-#include <array>
+#include <map>
 #include <iomanip>
 #include <ctime>
 
@@ -12,73 +12,39 @@
 /*
 Definiamo due tavole da gioco: CampoMine, che contiene le mine, e CampoGiocatore, che è quello che verrà stampato e aggiornato date le mosse del giocatore
 In entrambi i casi definiamo le due tavole come matrici template nell'implementazione "vettore" di "vettori".
-// TO DO: adattare l'implementazione di Padovani dei vettori sparsi per la matrice di mine?
 */
 
-/*
-typedef std::pair<bool, char> Cella;
-
-class Cella
+std::map<std::string, int > mappa_converti
 {
-private:
-	bool mina;
-	char stato; // n (normale), s (scavato), b (bandiera)
-public:
-	Cella(bool, char);												// costruttore
-	void Cambia_stato();
-	friend std::ostream& operator<<(std::ostream&, const Cella&);	// stampa cella su terminale
+	{"1", 1},
+	{"2", 2},
+	{"3", 3},
+	{"4", 4},
+	{"5", 5},
+	{"6", 6},
+	{"7", 7},
+	{"8", 8},
+	{u8"◉", -1},
+	{"-", 0},
+	{u8"⚑", -2},
+	{u8"⎕", -3},
 };
 
-Cella::Cella(bool m = false, char s = 'n')
+std::map<std::string, std::string > mappa_colori
 {
-	if (s != 'n' && s != 's' && s != 'b') throw std::domain_error("stato illegittimo");
-	mina = m;
-	stato = s;
-}
-
-std::ostream& operator<<(std::ostream& os, const Cella& c)
-{
-	os << "(" << c.first << ", " << c.second << ")";
-	return os;
-}
-
-class VettoreCella
-{
-private:
-	int lunghezza;				// lunghezza > 0
-	std::vector<Cella> data;
-public:
-	VettoreCella(int);														// costruttore
-	int dimensione() const { return lunghezza; };
-	Cella operator[](int) const;											// leggi elemento di data
-	Cella& operator[](int);													// scrivi elemento di data
-	friend std::ostream& operator<<(std::ostream&, const VettoreCella&);	// stampa vettore cella su terminale
+	{"1", "\x1B[38;2;1;0;254m"},
+	{"2", "\x1B[38;2;1;127;1m"},
+	{"3", "\x1B[38;2;254;0;0m"},
+	{"4", "\x1B[38;2;1;0;128m"},
+	{"5", "\x1B[38;2;129;1;2m"},
+	{"6", "\x1B[38;2;0;128;129m"},
+	{"7", "\x1B[38;2;0;0;0m"},
+	{"8", "\x1B[38;2;128;128;128m"},
+	{u8"◉", "\x1B[38;2;0;0;0m"},
+	{"-", "\x1B[38;2;0;0;0m"},
+	{u8"⚑", "\x1B[38;2;159;0;1m"},
+	{u8"⎕", "\x1B[38;2;0;0;0m"},
 };
-
-VettoreCella::VettoreCella(int l = 1)
-{
-	if (l < 1) throw std::domain_error("dimensioni vettore invalide");
-	lunghezza = l;
-	data.resize(l, Cella(0, 'n'));
-}
-
-Cella VettoreCella::operator[](int i) const
-{
-	return data.at(i);
-}
-
-Cella& VettoreCella::operator[](int i)
-{
-	return data.at(i);
-}
-
-std::ostream& operator<<(std::ostream& os, const VettoreCella& c)
-{
-	for (int i = 0; i < c.dimensione(); i++)
-		os << c[i];
-	return os;
-}
-*/
 
 template <typename T, int R, int C>
 class Campo
@@ -124,13 +90,36 @@ std::vector<T>& Campo<T, R, C>::operator[](int i)
 template <typename T, int R, int C>
 std::ostream& operator<<(std::ostream& os, const Campo<T, R, C>& c)
 {
-	for (int i = 0; i < c.righe; i++)
+	for (int i = 0; i < R; i++)
 	{
-		for (int j = 0; j < c.colonne; j++)
+		for (int j = 0; j < C; j++)
 		{
 			os << c[i][j];
 		}
 		os << std::endl;
+	}
+	return os;
+}
+
+template <int R, int C>
+std::ostream& operator<<(std::ostream& os, const Campo<std::string, R, C>& c)
+{
+	os << std::setw(3);
+	for (int j = 0; j < C; j++)
+	{
+		os << j+1;
+	}
+	os << std::endl;
+	//os << std::setw(2) << std::setfill('-') << std::setw(C) << std::endl;
+	for (int i = 0; i < R; i++)
+	{
+		os << i+1 << u8"│";
+		os << "\x1B[48;2;192;192;192m";
+		for (int j = 0; j < C; j++)
+		{
+			os << mappa_colori[c[i][j]] << c[i][j];
+		}
+		os << "\x1B[0m" << std::endl;
 	}
 	return os;
 }
@@ -141,7 +130,15 @@ void Campo<T, R, C>::reset() {
 	{
 		for (int j = 0; j < colonne; j++)
 		{
-			campo[i][j] = T();
+			if (std::is_same_v<T, std::string>)
+			{
+				campo[i][j] = u8"⎕";
+			}
+			else
+			{
+				campo[i][j] = T();
+			}
+			
 		}
 	}
 }
@@ -190,7 +187,7 @@ void campo_casuale2(Campo<bool, R, C>& campo, int mine)
 template <typename T, int R, int C>
 bool Campo<T, R, C>::nelCampo(int i, int j) const
 {
-	return i < 0 || i >= R || j < 0 || j >= C ? false : true;
+	return i >= 0 && i < R && j >= 0 && j < C;
 }
 
 template <int R, int C>
@@ -219,12 +216,85 @@ Campo<std::string, R, C> ConvertiCampo(const Campo<bool, R, C>& campo)
 	{
 		for (int j = 0; j < C; j++)
 		{
-			if (ContaMine(campo, i, j) == -1) res[i][j] = "*";
+			if (ContaMine(campo, i, j) == -1) res[i][j] = u8"◉";
 			else if (ContaMine(campo, i, j) == 0) res[i][j] = "-";
-			else res[i][j] = std::to_string(ContaMine(campo, i, j)); //
+			else res[i][j] = std::to_string(ContaMine(campo, i, j));
 		}
 	}
 	return res;
+}
+
+template <int R, int C>
+void cambia_stato(Campo<std::string, R, C>& campo_giocatore, const Campo<bool, R, C>& campo_gioco, int i, int j, char comando)
+{
+	if (comando != 'B' && comando != 'S') throw std::invalid_argument("comando illecito");
+	if (comando == 'B')
+	{
+		if (campo_giocatore[i][j] == u8"⚑")
+		{
+			campo_giocatore[i][j] = u8"⎕";
+			return;
+		}
+		else if (campo_giocatore[i][j] == u8"⎕")
+		{
+			campo_giocatore[i][j] = u8"⚑";
+			return;
+		}
+		else return;
+	}
+	else
+	{
+		if (campo_gioco[i][j])
+		{
+			campo_giocatore[i][j] = u8"◉";
+			return;
+		}
+		else if (campo_giocatore[i][j] == u8"⚑")
+		{
+			return;
+		}
+		else
+		{
+			if (ContaMine(campo_gioco, i, j) == 0) campo_giocatore[i][j] = "-";
+			else campo_giocatore[i][j] = std::to_string(ContaMine(campo_gioco, i, j));
+			//if (mappa_converti[campo_giocatore[i][j]] > 0) {
+			//	return;
+			//}
+			scava_celle(campo_giocatore, campo_gioco, i, j);
+			return;
+		}
+	}
+}
+
+template <int R, int C>
+void scava_celle(Campo<std::string, R, C>& campo_giocatore, const Campo<bool, R, C>& campo_gioco, int i, int j)
+{
+	if (campo_giocatore[i][j] != "-")
+	{
+		return;
+	}
+	
+	for (int n = i - 1; n <= i + 1; n++)
+	{
+		for (int m = j - 1; m <= j + 1; m++)
+		{
+			if (campo_giocatore.nelCampo(n, m))
+			{
+				if (campo_giocatore[n][m] == u8"⚑" || campo_gioco[n][m])
+				{
+					break;
+				}	
+				else if (campo_giocatore[n][m] == u8"⎕")
+				{
+					if (ContaMine(campo_gioco, n, m) == 0) campo_giocatore[n][m] = "-";
+					else campo_giocatore[n][m] = std::to_string(ContaMine(campo_gioco, n, m));
+
+					scava_celle(campo_giocatore, campo_gioco, n, m);
+				}
+			}
+		}
+	}
+	return;
 }
 
 /* La classe di Gioco */
@@ -250,6 +320,8 @@ void Gioco::rivela()
 	// ???
 }
 
+// TO DO: comando che interviene e termina la partita se vede in campo_giocatore un asterisco(metodo di Gioco)
+
 int main()
 {
 
@@ -263,59 +335,62 @@ int main()
 
 	std::srand(std::time(nullptr));
 
-	Campo<bool, 5, 5> c;
-	c[3][3] = 1; c[4][3] = 1; c[3][4] = 1;
-	std::cout << c << std::endl;
-	std::cout << ConvertiCampo(c) << std::endl;
-	/* test per verificare la "casualità" (molto poco da CPS) dei due metodi di generatozione*/
+	Campo<bool, 9, 9> c;
+	Campo<std::string, 9, 9> c_char;
+	c[0][6] = 1;
+	c[1][1] = 1;
+	c[2][0] = 1; c[2][5] = 1;
+	c[3][6] = 1;
+	c[5][5] = 1;
+	c[6][3] = 1;
+	c[7][4] = 1;
+	c[8][5] = 1; c[8][6] = 1;
+	for (int i = 0; i < 9; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			c_char[i][j] = u8"⎕";
+		}
+	}
+	//std::cout << c << std::endl;
+	//std::cout << ConvertiCampo(c) << std::endl;
+	//std::cout << c_char << std::endl;
+	int x;
+	int y;
+	char comando;
+	while (true)
+	{
+		std::cout << c_char << std::endl;
+		while (true)
+		{
+			std::cout << "Fai la tua mossa (x, y, infine comando), altrimenti scrivi un numero negativo per uscire o resettare." << std::endl;
+			std::cin >> x;
+			if (x < 0) break;
+			std::cin >> y;
+			std::cin >> comando;
+			cambia_stato(c_char, c, x-1, y-1, comando);
+			std::cout << c_char << std::endl;
+		}
+		std::cout << "Vuoi ricominciare la stessa partita (scrivi R) o uscire dal gioco (scrivi U)?\a" << std::endl;
+		do
+		{
+			std::cin >> comando;
+			if (comando != 'R' && comando != 'U') {
+				std::cout << "Comando non conosciuto, riprova!" << std::endl;
+			}
+		} while (comando != 'R' && comando != 'U');
+		if (comando == 'U') break;
+		c_char.reset();
+	}
+	//rgba(1, 0, 254, 255)
 	/*
-	double mean = 0;
-	int kk = 0;
-	while (kk <= 20) {
-	Campo<bool, 16, 16> c1;
-	Campo<bool, 16, 16> c2;
-	campo_casuale(c1, 40);
-	campo_casuale2(c2, 40);
-	std::cout << c1 << std::endl << c2 << std::endl;
-	int sum = 0;
-	int sum2 = 0;
-	for (int i = 0; i < c1._righe(); i++)
-	{
-		for (int j = 0; j < c1._colonne(); j++)
-		{
-			if (c1[i][j] == 1) sum++;
-			if (c1[i][j] == 1) sum2++;
-		}
-		mean =+ (double) sum / c1._colonne();
-		std::cout << (double) sum/ c1._colonne() << std::endl;
-		sum = 0;
-	}
-	std::cout << (double) mean / c1._righe() << std::endl;
-	std::cout << sum2 << std::endl;
-	sum2 = 0;
-	for (int i = 0; i < c2._righe(); i++)
-	{
-		for (int j = 0; j < c2._colonne(); j++)
-		{
-			if (c2[i][j] == 1) sum++;
-			if (c2[i][j] == 1) sum2++;
-		}
-		mean = +(double)sum / c2._colonne();
-		std::cout << (double)sum / c2._colonne() << std::endl;
-		sum = 0;
-	}
-	std::cout << (double)mean / c2._righe() << std::endl;
-	std::cout << sum2 << std::endl;
-	sum2 = 0;
-	system("PAUSE");
-	kk++;
-	system("CLS");
-	}
-	int sum;
-	std::cout << "\x1B[41mTexting\033[0m\t\t";
-	std::cout << u8"";
-	std::cout << u8"⚐";
-	std::cout << u8"⚑";
+	std::cout << "\x1B[48;2;192;192;192m\x1B[38;2;1;0;254m1\033[0m" << std::endl;		// 1 - blu chiaro
+	std::cout << "\x1B[48;2;192;192;192m\x1B[38;2;1;127;1m2\033[0m" << std::endl;		// 2 - verde
+	std::cout << "\x1B[48;2;192;192;192m\x1B[38;2;254;0;0m3\033[0m" << std::endl;		// 3 - rosso
+	std::cout << "\x1B[48;2;192;192;192m\x1B[38;2;1;0;128m4\033[0m" << std::endl;		// 4 - blu scuro
+	std::cout << "\x1B[48;2;192;192;192m\x1B[38;2;129;1;2m5\033[0m" << std::endl;		// 5 - amaranto
+	std::cout << "\x1B[48;2;192;192;192m\x1B[38;2;0;128;129m6\033[0m" << std::endl;		// 6 - turchese
+	std::cout << "\x1B[48;2;192;192;192m\x1B[38;2;0;0;0m7\033[0m" << std::endl;			// 7 - nero
+	std::cout << "\x1B[48;2;192;192;192m\x1B[38;2;128;128;128m8\033[0m" << std::endl;	// 8 - grigio scurino
 	*/
-
 }
